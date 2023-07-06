6a9e43252dc295fab3655d9c0b094bddfeb8ee3f
Ezequiel Amin <ezequielamin@outlook.com>
2023-07-06 15:52:26 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+console.log('Hola mundo');
+
+let usuario = 'Pepe';
+usuario = 'Juan';
+usuario = 5;
+
+console.log(usuario);
+
+// let respuestaUsuario;
+// respuestaUsuario = prompt("¿Cual es tu nombre?")
+
+// console.log(respuestaUsuario)
+
+document.write("hola")
+
+let respuestaUsuario;
+// respuestaUsuario = prompt("Ingrese su nombre")
+
+// alert("el nombre que ingresaste es : " + respuestaUsuario);
+// console.log("el nombre que ingresaste es : " + respuestaUsuario);
+
+let hola;
+
+console.log(2 + {})
